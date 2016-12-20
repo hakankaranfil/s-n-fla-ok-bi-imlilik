b6519282dabdf9d17aaffbe5cd39872ba4401230
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2016-12-20 09:28:55 +0200

    finish

---
+++ sınıflar/sınıflar/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınıflar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ögrenci ögr1 = new ögrenci();
+        ögrenci ögr2 = new ögrenci();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ögr1.adsoyad = "hakan karanfil";
+            ögr2.adsoyad = "emre karafil";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("isim" + ögr1.adsoyad);
+            MessageBox.Show("isim" + ögr2.adsoyad);
+        }
+    }
+}
